81767c64ebdb0bcd7e21ecb9d30eb526e8aac7fa
kireeti-28 <kireeti428@gmail.com>
2025-08-07 05:03:35 +0000

    main

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+
+function main() {
+    const args = process.argv;
+
+    if (args.length !== 3) {
+        console.error("invalid args no of args");
+        process.exit(1);
+    }
+
+    const baseURL = args[2];
+    console.log(`starting crawl of: ${baseURL}`);
+
+    process.exit(0);
+}
+
+main();
