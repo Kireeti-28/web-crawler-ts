75e7009be8d7c1ef559225ec2a07219acd5c255c
kireeti-28 <kireeti428@gmail.com>
2025-08-07 04:53:37 +0000

    url extraction

--- src/crawl.test.ts
+++ src/crawl.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest';
-import { normalizeURL } from './crawl';
+import { getURLsFromHTML, normalizeURL } from './crawl';
 
 test('normalizeURL test https', () => {
     const input = "https://myfriends.com";
@@ -48,4 +48,34 @@ test('normalizeURL test caps', () => {
     const actual = normalizeURL(input);
     const expected = "myfriends.com";
     expect(actual).toBe(expected);
+});
+
+test("getURLsFromHTML absolute", () => {
+  const inputURL = "https://blog.boot.dev";
+  const inputBody =
+    '<html><body><a href="https://blog.boot.dev"><span>Boot.dev></span></a></body></html>';
+  const actual = getURLsFromHTML(inputBody, inputURL);
+  const expected = ["https://blog.boot.dev/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", () => {
+  const inputURL = "https://blog.boot.dev";
+  const inputBody =
+    '<html><body><a href="/path/one"><span>Boot.dev></span></a></body></html>';
+  const actual = getURLsFromHTML(inputBody, inputURL);
+  const expected = ["https://blog.boot.dev/path/one"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML both", () => {
+  const inputURL = "https://blog.boot.dev";
+  const inputBody =
+    '<html><body><a href="/path/one"><span>Boot.dev></span></a><a href="https://other.com/path/one"><span>Boot.dev></span></a></body></html>';
+  const actual = getURLsFromHTML(inputBody, inputURL);
+  const expected = [
+    "https://blog.boot.dev/path/one",
+    "https://other.com/path/one",
+  ];
+  expect(actual).toEqual(expected);
 });
